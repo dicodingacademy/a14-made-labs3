a4b88f3c4ecbc9d28b1534cd4485d88eb68d9974
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-04-16 15:49:28 +0700

    Add Idle Resource

---
+++ 10.Idle Resource/MyIdleResource/app/src/main/java/com/dicoding/picodiploma/myidleresource/MainActivity.java
@@ -0,0 +1,40 @@
+package com.dicoding.picodiploma.myidleresource;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EspressoIdlingResource.increment();
+
+        ((TextView) findViewById(R.id.text_view)).setText(getString(R.string.start));
+
+        EspressoIdlingResource.decrement();
+
+        EspressoIdlingResource.increment();
+
+        delay();
+    }
+
+    private void delay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ((TextView) findViewById(R.id.text_view)).setText(getString(R.string.finish));
+
+                if (!EspressoIdlingResource.getEspressoIdlingResourcey().isIdleNow()) {
+                    //Memberitahukan bahwa tugas sudah selesai dijalankan
+                    EspressoIdlingResource.decrement();
+                }
+            }
+        }, 2000);
+    }
+}
