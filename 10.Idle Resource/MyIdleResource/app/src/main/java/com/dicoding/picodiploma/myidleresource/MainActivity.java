0b03ea88e5cda13b15b9f7524f99ea51da1dd921
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-04-16 17:00:41 +0700

    Add Thread

--- 10.Idle Resource/MyIdleResource/app/src/main/java/com/dicoding/picodiploma/myidleresource/MainActivity.java
+++ 10.Idle Resource/MyIdleResource/app/src/main/java/com/dicoding/picodiploma/myidleresource/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,13 +34,31 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                ((TextView) findViewById(R.id.text_view)).setText(getString(R.string.finish));
+                textView.setText(getString(R.string.loading));
+
+                if (!EspressoIdlingResource.getEspressoIdlingResourcey().isIdleNow()) {
+                    //Memberitahukan bahwa tugas sudah selesai dijalankan
+                    EspressoIdlingResource.decrement();
+                }
+
+                EspressoIdlingResource.increment();
+
+                loadData();
+            }
+        }, 2000);
+    }
+
+    private void loadData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(getString(R.string.finish));
 
                 if (!EspressoIdlingResource.getEspressoIdlingResourcey().isIdleNow()) {
                     //Memberitahukan bahwa tugas sudah selesai dijalankan
                     EspressoIdlingResource.decrement();
                 }
             }
-        }, 2000);
+        }, 3000);
     }
 }
