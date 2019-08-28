5422d4b7b8b115528fbec696a6d37cd14b81c735
Arif <faizin.arief@gmail.com>
2019-08-28 11:03:19 +0700

    Update Java Codacy MyDeepNavigation

--- Navigation/MyDeepNavigation/java/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/DetailActivity.java
+++ Navigation/MyDeepNavigation/java/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/DetailActivity.java
@@ -1,11 +1,11 @@
 package com.dicoding.picodiploma.mydeepnavigation;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-    TextView tvTitle, tvMessage;
+
     public static final String EXTRA_TITLE = "extra_title";
     public static final String EXTRA_MESSAGE = "extra_message";
 
@@ -14,8 +14,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        tvTitle = findViewById(R.id.tv_title);
-        tvMessage = findViewById(R.id.tv_message);
+        TextView tvTitle = findViewById(R.id.tv_title);
+        TextView tvMessage = findViewById(R.id.tv_message);
 
         /*
         Ambil data dari intent yang dikirimkan oleh notifikasi
--- Navigation/MyDeepNavigation/java/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/MainActivity.java
+++ Navigation/MyDeepNavigation/java/app/src/main/java/com/dicoding/picodiploma/mydeepnavigation/MainActivity.java
@@ -11,10 +11,10 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private static class DelayAsync extends AsyncTask<Void, Void, Void> {
 
-        WeakReference<AsyncCallback> callback;
+        final WeakReference<AsyncCallback> callback;
 
         DelayAsync(AsyncCallback callback) {
 
