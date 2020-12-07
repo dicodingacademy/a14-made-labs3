fdf82bed8a30894fb126788328568e519b5794bd
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-12-07 15:20:33 +0700

    [kotlin] reformat code

--- Broadcast/MyBroadcastReceiver/kotlin/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiver.kt
+++ Broadcast/MyBroadcastReceiver/kotlin/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiver.kt
@@ -10,7 +10,9 @@ import android.util.Log
 
 class SmsReceiver : BroadcastReceiver() {
 
-    private val TAG = SmsReceiver::class.java.simpleName
+    companion object {
+        private val TAG = SmsReceiver::class.java.simpleName
+    }
 
     override fun onReceive(context: Context, intent: Intent) {
         val bundle = intent.extras
@@ -19,9 +21,9 @@ class SmsReceiver : BroadcastReceiver() {
                 /*
                 Bundle dengan key "pdus" sudah merupakan standar yang digunakan oleh system
                  */
-                val pdusObj = bundle.get("pdus") as Array<Any>
+                val pdusObj = bundle.get("pdus") as Array<*>
                 for (aPdusObj in pdusObj) {
-                    val currentMessage = getIncomingMessage(aPdusObj, bundle)
+                    val currentMessage = getIncomingMessage(aPdusObj as Any, bundle)
                     val senderNum = currentMessage.displayOriginatingAddress
                     val message = currentMessage.displayMessageBody
                     Log.d(TAG, "senderNum: $senderNum; message: $message")
@@ -33,16 +35,16 @@ class SmsReceiver : BroadcastReceiver() {
                 }
             }
         } catch (e: Exception) {
-            Log.d(TAG, "Exception smsReceiver$e")
+            Log.d(TAG, "Exception smsReceiver $e")
         }
     }
 
     private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
         val currentSMS: SmsMessage
-        if (Build.VERSION.SDK_INT >= 23) {
-            val format = bundle.getString("format")
-            currentSMS = SmsMessage.createFromPdu(aObject as ByteArray, format)
-        } else currentSMS = SmsMessage.createFromPdu(aObject as ByteArray)
+        val format = bundle.getString("format")
+        currentSMS = if (Build.VERSION.SDK_INT >= 23) {
+            SmsMessage.createFromPdu(aObject as ByteArray, format)
+        } else SmsMessage.createFromPdu(aObject as ByteArray)
         return currentSMS
     }
 }
