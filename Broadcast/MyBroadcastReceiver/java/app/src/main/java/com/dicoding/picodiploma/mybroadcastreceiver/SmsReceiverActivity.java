3e00bf5c056c41305d8e482708faf9efb4fc9c20
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-12-07 16:12:40 +0700

    [java] refactor to viewBinding

--- Broadcast/MyBroadcastReceiver/java/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiverActivity.java
+++ Broadcast/MyBroadcastReceiver/java/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/SmsReceiverActivity.java
@@ -1,42 +1,39 @@
 package com.dicoding.picodiploma.mybroadcastreceiver;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class SmsReceiverActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView tvSmsFrom;
-    private TextView tvSmsMessage;
-    private Button btnClose;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dicoding.picodiploma.mybroadcastreceiver.databinding.ActivitySmsReceiverBinding;
+
+public class SmsReceiverActivity extends AppCompatActivity {
 
     public static final String EXTRA_SMS_NO = "extra_sms_no";
     public static final String EXTRA_SMS_MESSAGE = "extra_sms_message";
 
+    private ActivitySmsReceiverBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sms_receiver);
+
+        binding = ActivitySmsReceiverBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         setTitle(getString(R.string.incoming_message));
 
-        tvSmsFrom = findViewById(R.id.tv_from);
-        tvSmsMessage = findViewById(R.id.tv_message);
-        btnClose = findViewById(R.id.btn_close);
-        btnClose.setOnClickListener(this);
+        binding.btnClose.setOnClickListener(v -> finish());
 
         String senderNo = getIntent().getStringExtra(EXTRA_SMS_NO);
         String senderMessage = getIntent().getStringExtra(EXTRA_SMS_MESSAGE);
 
-        tvSmsFrom.setText(String.format("from : %s", senderNo));
-        tvSmsMessage.setText(senderMessage);
+        binding.tvFrom.setText(String.format("from : %s", senderNo));
+        binding.tvMessage.setText(senderMessage);
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_close) {
-            finish();
-        }
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
