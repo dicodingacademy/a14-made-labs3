98fbc20229040ed8b1b864f0ba07a271dc6d05e8
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-12-07 16:13:24 +0700

    [java] resolve intent service deprecated

--- Broadcast/MyBroadcastReceiver/java/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/DownloadService.java
+++ Broadcast/MyBroadcastReceiver/java/app/src/main/java/com/dicoding/picodiploma/mybroadcastreceiver/DownloadService.java
@@ -1,31 +1,35 @@
 package com.dicoding.picodiploma.mybroadcastreceiver;
 
-import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.JobIntentService;
 
-public class DownloadService extends IntentService {
+
+public class DownloadService extends JobIntentService {
 
     public static final String TAG = DownloadService.class.getSimpleName();
 
-    public DownloadService() {
-        super("DownloadService");
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        if (intent != null) {
+            enqueueWork(this, this.getClass(), 101, intent);
+        }
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleWork(@NonNull Intent intent) {
         Log.d(TAG, "Download Service dijalankan");
-        if (intent != null) {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            Intent notifyFinishIntent = new Intent(MainActivity.ACTION_DOWNLOAD_STATUS);
-            sendBroadcast(notifyFinishIntent);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
 
+        Intent notifyFinishIntent = new Intent(MainActivity.ACTION_DOWNLOAD_STATUS);
+        sendBroadcast(notifyFinishIntent);
+    }
 }
